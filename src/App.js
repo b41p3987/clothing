6d2f52646959a38e4a6b693f813766f273aa83d3
chenyuyinggit config --global user.name chenyuyinggit config --global user.email as112233ppq2012@gmail.com <as112233ppq2012@gmail.com>
2020-09-24 13:10:34 +0800

    adding signin button component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import ShopPage from './pages/shop/shop.component.jsx';
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
